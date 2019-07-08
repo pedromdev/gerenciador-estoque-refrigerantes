22450d0e9bbebc98d43b4d20a98f26aeb9104495
Pedro Alves <pedro.alves@bannet.com.br>
2019-07-08 14:06:56 -0300

    Retornando objetos com métodos de acesso e verificação dos erros

--- resources/js/store/getters/index.js
+++ resources/js/store/getters/index.js
@@ -1,10 +1,24 @@
 const getterPorCampo = campo => state => state[campo];
 
 export default {
-  erros: getterPorCampo('erros'),
+  erros: state => ({
+    tem(campo) {
+      if (!campo) {
+        return Object.keys(state.erros).filter(campo => state.erros[campo].length > 0).length > 0;
+      }
+      return !!state.erros[campo] && state.erros[campo].length > 0;
+    },
+    primeiro(campo) {
+      return state.erros[campo].find(v => !!v);
+    },
+    todos(campo) {
+      return state.erros[campo];
+    }
+  }),
   usuario: getterPorCampo('usuario'),
   marca: getterPorCampo('marca'),
   marcas: getterPorCampo('marcas'),
   refrigerante: getterPorCampo('refrigerante'),
   refrigerantes: getterPorCampo('refrigerantes'),
 }
+
