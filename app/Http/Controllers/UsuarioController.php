e509e6419b367b0dc34cbb045e5cbd4bc5493255
Pedro Alves <pedromarcelodesaalves@gmail.com>
2019-07-08 22:21:34 -0300

    Traduzindo mensagens de validação dos dados

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -15,6 +15,22 @@ class UsuarioController extends Controller
     use ErrosValidacao, DadosRequisicao, ManipularModel;
 
     /**
+     * @var array
+     */
+    private $messages = [
+        'name.required' => 'O campo nome é obrigatório.',
+        'name.max' => 'O campo nome não pode ter mais do que 255 caracteres.',
+        'email.required' => 'O campo e-mail é obrigatório.',
+        'email.email' => 'O campo e-mail deve ser um endereço de e-mail válido.',
+        'email.unique' => 'Este e-mail já está em uso.',
+        'password.required' => 'O campo senha é obrigatório.',
+        'password.confirmed' => 'As senhas não são iguais.',
+        'password.min' => 'O campo senha deve ter pelo menos 6 caracteres.',
+        'password_confirmation.required' => 'O campo confirmar senha é obrigatório.',
+        'password_confirmation.min' => 'O campo confirmar senha deve ter pelo menos 6 caracteres.',
+    ];
+
+    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -25,8 +41,9 @@ class UsuarioController extends Controller
         $validador = Validator::make($request->all(), [
             'name' => 'required|max:255',
             'email' => 'required|email|unique:users',
-            'password' => 'required|confirmed|min:6'
-        ]);
+            'password' => 'required|confirmed|min:6',
+            'password_confirmation' => 'required|min:6',
+        ], $this->messages);
 
         if ($validador->fails()) {
             return $this->retornarErrosDoValidador($validador);
@@ -68,7 +85,7 @@ class UsuarioController extends Controller
             'email' => "email|unique:users,email,$user->id",
             'password' => 'confirmed|min:6',
             'password_confirmation' => 'min:6',
-        ]);
+        ], $this->messages);
 
         if ($validador->fails()) {
             return $this->retornarErrosDoValidador($validador);
