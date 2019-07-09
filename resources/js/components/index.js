e62ba1b244d6d08def1878058856874110c67714
Pedro Alves <pedromarcelodesaalves@gmail.com>
2019-07-09 00:17:58 -0300

    Iniciando layout do sistema

--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -3,7 +3,11 @@ import Vue from 'vue';
 import EntrarForm from './Form/Entrar';
 import CadastrarForm from './Form/Cadastrar';
 import Field from "./Form/Utils/Field";
+import Navbar from "./Layout/Navbar";
+import Layout from "./Layout";
 
 Vue.component('field', Field);
 Vue.component('entrar-form', EntrarForm);
 Vue.component('cadastrar-form', CadastrarForm);
+Vue.component('navbar', Navbar);
+Vue.component('layout', Layout);
