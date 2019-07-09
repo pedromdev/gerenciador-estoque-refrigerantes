cd1e293a0dfa01011091f56ee675a7c86f626ff3
Pedro Alves <pedromarcelodesaalves@gmail.com>
2019-07-08 22:25:18 -0300

    Componente para renderização dos erros do campo de formulário

---
+++ resources/js/components/Form/Utils/Field.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="field" v-bind:class="{ error: erros.tem(name) || errorsBag.has(name) }">
+    <slot></slot>
+    <label v-if="erros.tem(name)">{{ erros.primeiro(name) }}</label>
+    <label v-if="errorsBag.has(name)">{{ errorsBag.first(name) }}</label>
+  </div>
+</template>
+
+<script>
+  import {mapGetters} from 'vuex';
+
+  export default {
+    name: "Field",
+    props: [ 'name', 'errorsBag' ],
+    computed: {
+      ...mapGetters([ 'erros' ]),
+    }
+  }
+</script>
--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 
 import EntrarForm from './Form/Entrar';
 import CadastrarForm from './Form/Cadastrar';
+import Field from "./Form/Utils/Field";
 
+Vue.component('field', Field);
 Vue.component('entrar-form', EntrarForm);
 Vue.component('cadastrar-form', CadastrarForm);
