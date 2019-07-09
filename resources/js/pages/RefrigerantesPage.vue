77163a1a3578d8297610ef0ca5203019096c2aab
Pedro Alves <pedromarcelodesaalves@gmail.com>
2019-07-09 00:16:36 -0300

    Listando refrigerantes cadastrados no componente

--- resources/js/pages/RefrigerantesPage.vue
+++ resources/js/pages/RefrigerantesPage.vue
@@ -3,7 +3,23 @@
 </template>
 
 <script>
+  import {mapGetters, mapActions} from 'vuex';
+
   export default {
     name: "RefrigerantesPage",
+    mounted() {
+      document.title = 'Lista de refrigerantes | Gerenciador de estoque';
+      this.listarRefrigerantes();
+    },
+    methods: {
+      ...mapActions([
+        'listarRefrigerantes'
+      ]),
+    },
+    computed: {
+      ...mapGetters([
+        'refrigerantes'
+      ])
+    }
   }
 </script>
