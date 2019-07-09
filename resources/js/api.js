f2c67fc4c4039d3137eb79fd6e90b467f52eecb7
Pedro Alves <pedromarcelodesaalves@gmail.com>
2019-07-08 22:24:31 -0300

    Adicionando header com o token de acesso em todos os endpoints

--- resources/js/api.js
+++ resources/js/api.js
@@ -5,15 +5,13 @@ axios.auth = () => {
 
   const token = JSON.parse(localStorage.getItem('token'));
 
-  const instance = axios.create({
-    baseURL: '/api',
-    headers: { Authorization: `Bearer ${token.access_token}` }
-  });
+  const headers = { Authorization: `Bearer ${token.access_token}` };
+  const instance = axios.create({ baseURL: '/api', headers });
 
-  instance.autenticacao = axios.create({ baseURL: '/api/autenticacao' });
-  instance.usuarios = axios.create({ baseURL: '/api/usuarios' });
-  instance.marcas = axios.create({ baseURL: '/api/marcas' });
-  instance.refrigerantes = axios.create({ baseURL: '/api/refrigerantes' });
+  instance.autenticacao = axios.create({ baseURL: '/api/autenticacao', headers });
+  instance.usuarios = axios.create({ baseURL: '/api/usuarios', headers });
+  instance.marcas = axios.create({ baseURL: '/api/marcas', headers });
+  instance.refrigerantes = axios.create({ baseURL: '/api/refrigerantes', headers });
 
   return instance;
 };
