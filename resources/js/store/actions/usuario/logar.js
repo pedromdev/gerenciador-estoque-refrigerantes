f643fc503b7f986dda466943f56b66e183c9d96e
Pedro Alves <pedromarcelodesaalves@gmail.com>
2019-07-09 21:52:41 -0300

    Inserindo a mensagem de erro de login quando o status da requisição for 401

--- resources/js/store/actions/usuario/logar.js
+++ resources/js/store/actions/usuario/logar.js
@@ -8,5 +8,13 @@ export default ({ dispatch, commit }, credenciais) => {
       localStorage.setItem('token', JSON.stringify(token));
       dispatch('pegarUsuario');
       router.push('/');
-    }).catch(e => commit('exibirErros', e.response.data));
+    }).catch(e => {
+      if (e.response.status === 401) {
+        commit('exibirErros', {
+          email: [ 'E-mail e/ou senha incorretos' ]
+        })
+      } else {
+        commit('exibirErros', e.response.data)
+      }
+    });
 }
